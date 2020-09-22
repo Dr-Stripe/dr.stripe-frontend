04933ee3a4c7cd3997693cc395003f364f90edbc
ryutamura <rtamura@bluecatnetworks.com>
2020-09-22 20:01:03 +0900

    added json files and modified UnpaidList.jsx

--- src/UnpaidList.jsx
+++ src/UnpaidList.jsx
@@ -1,7 +1,17 @@
 //This is List for unpaid component
 // It should be default inter face
 import React from "react";
+import visits from "./visits.json";
+import patiants from "./patiants.json";
 
-export default function UnpainList({ data }) {
+
+export default function UnpaidList({ data }) {
+
+  function showUnpaid() {
+    for (const visit of visits) {
+      
+    }
+  }
+
   return <div></div>;
 }
